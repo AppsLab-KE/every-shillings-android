49e7633e6048cd42857ab25ac4f795f798911e5f
moemaair <rageali12@gmail.com>
2023-03-20 13:24:59 +0300

    fixes on Onboading Screen

--- app/src/main/java/com/appslabke/every_shillings_android/ui/screens/OnboadingScreen.kt
+++ app/src/main/java/com/appslabke/every_shillings_android/ui/screens/OnboadingScreen.kt
@@ -16,72 +16,63 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.appslabke.every_shillings_android.R
 
-
 @Composable
 fun OnboadingScreen(
     navigateToLoginScreen: () -> Unit,
     navigateToSigUpScreen: () -> Unit
 ) {
-    Surface {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = 32.dp, vertical = 147.dp),
-            contentAlignment = Alignment.Center,
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize().padding(horizontal = 33.dp),
+            verticalArrangement = Arrangement.SpaceAround
         ) {
-            Box(modifier = Modifier.align(Alignment.TopStart)){
-                Column(modifier = Modifier.fillMaxWidth().wrapContentSize(align = Alignment.Center)) {
-                    Image(painter =
-                    painterResource(id = R.drawable.dummy_logo),
-                        contentDescription = "",
-                        modifier = Modifier.size(115.dp, 160.dp))
-                    Spacer(modifier = Modifier.height(138.dp) )
-                }
-            }
-            Box(modifier = Modifier
-                .padding(vertical = 15.dp)
-                .align(Alignment.Center),
-            ){
-                Column() {
-                    Text(text = "Welcome to Everyshillingi", modifier = Modifier
-                        .fillMaxWidth(), textAlign = TextAlign.Center,
-                        fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
-                    Spacer(modifier = Modifier.height(20.dp) )
-                    Text(text = "Exchange made easy, anywhere.", modifier = Modifier
-                        .fillMaxWidth(),textAlign = TextAlign.Center, fontSize = 14.sp)
-                }
-            }
-            Box(modifier = Modifier.align(Alignment.BottomCenter)){
-                Column(modifier = Modifier) {
-
-                    // Sign up
-                    Button(onClick = { navigateToSigUpScreen() },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp),
-                        shape = MaterialTheme.shapes.medium,
-                        colors = ButtonDefaults.buttonColors(
-                            backgroundColor = Color(0xFF2B5EC0)
-                        )
-
-                    ) {
-                        Text(text = "Sign up")
-                    }
-                    Spacer(modifier = Modifier.height(16.dp))
-
-                    // Sign in
-                    OutlinedButton(onClick = { navigateToLoginScreen() },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(56.dp),
-                        shape = MaterialTheme.shapes.medium,
-                        border = BorderStroke(1.dp, color = Color(0xFF2B5EC0))
-                    ) {
-                        Text(text = "Sign in")
-                    }
-                }
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(align = Alignment.Center)) {
+                Image(painter =
+                painterResource(id = R.drawable.dummy_logo),
+                    contentDescription = "",
+                    modifier = Modifier.size(115.dp, 160.dp))
 
             }
+
+            Column() {
+                Text(text = "Welcome to Everyshilling", modifier = Modifier
+                    .fillMaxWidth(), textAlign = TextAlign.Center,
+                    fontSize = 24.sp, fontWeight = FontWeight.SemiBold)
+                Spacer(modifier = Modifier.height(15.dp) )
+                Text(text = "Exchange made easy, anywhere.", modifier = Modifier
+                    .fillMaxWidth(),textAlign = TextAlign.Center, fontSize = 14.sp)
+            }
+
+            Column(modifier = Modifier) {
+
+                // Sign up
+                Button(onClick = {  navigateToSigUpScreen()},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    shape = MaterialTheme.shapes.medium,
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color(0xFF2B5EC0)
+                    )
+
+                ) {
+                    Text(text = "Sign up")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+                // Sign in
+                OutlinedButton(onClick = { navigateToLoginScreen() },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(56.dp),
+                    shape = MaterialTheme.shapes.medium,
+                    border = BorderStroke(1.dp, color = Color(0xFF2B5EC0))
+                ) {
+                    Text(text = "Sign in")
+                }
+            }
+
         }
     }
 }

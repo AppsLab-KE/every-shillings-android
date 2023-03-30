49a7f1700435dc8d66dbb016d7c305aaacade608
jacqui <gitaujaquiline@gmail.com>
2023-03-31 00:08:53 +0300

    Modifying List View to Lazy Column

---
+++ app/src/main/java/com/appslabke/every_shillings_android/composables/DropDownMenu.kt
@@ -0,0 +1,76 @@
+package com.appslabke.every_shillings_android.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.appslabke.every_shillings_android.R
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DropDownMenu(
+    modifier: Modifier = Modifier.fillMaxWidth(1f),
+    list: List<String>,
+    selected: String = list.first(),
+    onItemSelected: (String) -> Unit
+) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    ExposedDropdownMenuBox(modifier = modifier
+        .fillMaxWidth()
+        .background(Color.White)
+        .padding(horizontal = 16.dp),
+        expanded = expanded,
+        onExpandedChange = {
+            expanded = expanded.not()
+        }) {
+        // text field
+        TextField(
+            value = selected,
+            onValueChange = {},
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .border(
+                    width = 1.dp, Color(0xFF2B5EC0), shape = RoundedCornerShape(10.dp)
+                ),
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = ExposedDropdownMenuDefaults.textFieldColors(backgroundColor = Color.Transparent)
+        )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = expanded.not() }) {
+            list.forEach { selectedOption ->
+                DropdownMenuItem(onClick = {
+                    onItemSelected.invoke(selectedOption)
+                    expanded = expanded.not()
+                }) {
+                    Text(
+                        text = selectedOption,
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                        fontWeight = FontWeight.W400,
+                        fontSize = 20.sp
+                    )
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/appslabke/every_shillings_android/composables/OfferCard.kt
@@ -0,0 +1,113 @@
+package com.appslabke.every_shillings_android.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.appslabke.every_shillings_android.R
+
+
+@Preview(showBackground = true)
+@Composable
+fun OfferCard() {
+    Card(
+        modifier = Modifier
+            .padding(16.dp)
+            .background(Color.White)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(10.dp),
+        elevation = 4.dp
+    )
+    {
+        Column(modifier = Modifier.padding(16.dp))
+        {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(
+                    "$1000.00",
+                    modifier = Modifier
+                        .padding(start = 11.dp, top = 7.dp),
+                    fontWeight = FontWeight.W400,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    fontSize = 24.sp,
+                    color = Color(0xFF2B5EC0)
+                )
+
+                Image(
+                    modifier = Modifier
+                        .width(29.2.dp)
+                        .height(13.94.dp),
+                    painter = painterResource(id = R.drawable.exchange_green_arrow),
+                    contentDescription = "Exchange Green Arrow"
+                )
+
+                Text(
+                    "$76.85",
+                    modifier = Modifier
+                        .padding(end = 11.dp, top = 7.dp),
+                    fontWeight = FontWeight.W400,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    fontSize = 24.sp,
+                    color = Color(0xFF2B5EC0)
+                )
+            }
+
+            Spacer(modifier = Modifier.height(2.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            )
+            {
+                Text(
+                    "$1.00 = €0.77",
+                    modifier = Modifier
+                        .padding(start = 11.dp, top = 7.dp),
+                    fontWeight = FontWeight.W300,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    fontSize = 18.sp,
+                    color = Color(0xFF000000)
+                )
+
+                Text(
+                    "Today",
+                    modifier = Modifier,
+                    fontWeight = FontWeight.W300,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    fontSize = 18.sp,
+                    color = Color(0xFF000000)
+                )
+
+                Text(
+                    "€1.00 = $1.31",
+                    modifier = Modifier
+                        .padding(end = 11.dp, top = 7.dp),
+                    fontWeight = FontWeight.W300,
+                    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+                    fontSize = 18.sp,
+                    color = Color(0xFF000000)
+                )
+
+            }
+        }
+    }
+}
+

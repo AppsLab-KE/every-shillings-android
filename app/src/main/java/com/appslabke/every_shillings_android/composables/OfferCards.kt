172b9715941f071da917dacc6eaf9f8f00d181ff
jacqui <gitaujaquiline@gmail.com>
2023-03-31 23:56:53 +0300

    Centering the "No Offers" text in the Market place UI

--- app/src/main/java/com/appslabke/every_shillings_android/composables/OfferCards.kt
+++ app/src/main/java/com/appslabke/every_shillings_android/composables/OfferCards.kt
@@ -1,11 +1,23 @@
 package com.appslabke.every_shillings_android.composables
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.appslabke.every_shillings_android.R
 
 @Composable
 fun OfferCards(
@@ -14,7 +26,18 @@ fun OfferCards(
 
     Column(modifier = modifier) {
         if (!hasOffers)
-            Text("No Offers")
+            Text(
+                "No Offers",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .wrapContentHeight(),
+                fontWeight = FontWeight.W400,
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 24.sp,
+                fontFamily = FontFamily(Font(R.font.poppins_regular))
+            )
         else
             LazyColumn() {
                 items(items = (0..5).toList()) { OfferCard() }

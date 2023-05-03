0dc6a392f1fb8d17d520c1031eae386aafe8952e
moemaair <rageali12@gmail.com>
2023-05-03 23:32:17 +0300

    Reference contants credentials variable for datadog

--- app/src/main/java/com/appslabke/every_shillings_android/MainApplication.kt
+++ app/src/main/java/com/appslabke/every_shillings_android/MainApplication.kt
@@ -2,6 +2,8 @@ package com.appslabke.every_shillings_android
 
 import android.app.Activity
 import android.app.Application
+import com.appslabke.every_shillings_android.Constants.APP_ID
+import com.appslabke.every_shillings_android.Constants.CLIENT_TOKEN
 import com.datadog.android.Datadog
 import com.datadog.android.DatadogSite
 import com.datadog.android.core.configuration.Configuration
@@ -13,45 +15,43 @@ import com.datadog.android.rum.tracking.ActivityViewTrackingStrategy
 import com.datadog.android.rum.tracking.ComponentPredicate
 
 class MainApplication : Application() {
-//    override fun onCreate() {
-//        super.onCreate()
-//        initializeDatadog()
-//    }
-//
-//    private fun initializeDatadog() {
-////        val clientToken = BuildConfig.CLIENT_TOKEN
-////        val applicationId = BuildConfig.APP_ID
-//        val envName = BuildConfig.BUILD_TYPE
-//        val appVariance = ""
-//
-//        val configuration = Configuration.Builder(
-//            tracesEnabled = true,
-//            rumEnabled = true,
-//            logsEnabled = true,
-//            crashReportsEnabled = true
-//        )
-//            .trackInteractions()
-//            .trackLongTasks(250L)
-//            .useViewTrackingStrategy(
-//                ActivityViewTrackingStrategy(
-//                    trackExtras = true,
-//                    componentPredicate = object : ComponentPredicate<Activity> {
-//                        override fun accept(component: Activity): Boolean {
-//                            return component !is MainActivity
-//                        }
-//
-//                        override fun getViewName(component: Activity): String? {
-//                            return null
-//                        }
-//
-//                    }
-//                )
-//            )
-//            .useSite(DatadogSite.US5)
-//            .build()
-//
-//        //val credentials = Credentials(clientToken, envName, appVariance, applicationId)
-//        Datadog.initialize(this, credentials, configuration, TrackingConsent.GRANTED)
-//        GlobalRum.registerIfAbsent(RumMonitor.Builder().build())
-//    }
+    override fun onCreate() {
+        super.onCreate()
+        initializeDatadog()
+    }
+
+    private fun initializeDatadog() {
+        val envName = BuildConfig.BUILD_TYPE
+        val appVariance = ""
+
+        val configuration = Configuration.Builder(
+            tracesEnabled = true,
+            rumEnabled = true,
+            logsEnabled = true,
+            crashReportsEnabled = true
+        )
+            .trackInteractions()
+            .trackLongTasks(250L)
+            .useViewTrackingStrategy(
+                ActivityViewTrackingStrategy(
+                    trackExtras = true,
+                    componentPredicate = object : ComponentPredicate<Activity> {
+                        override fun accept(component: Activity): Boolean {
+                            return component !is MainActivity
+                        }
+
+                        override fun getViewName(component: Activity): String? {
+                            return null
+                        }
+
+                    }
+                )
+            )
+            .useSite(DatadogSite.US5)
+            .build()
+
+        val credentials = Credentials(CLIENT_TOKEN, envName, appVariance, APP_ID)
+        Datadog.initialize(this, credentials, configuration, TrackingConsent.GRANTED)
+        GlobalRum.registerIfAbsent(RumMonitor.Builder().build())
+    }
 }

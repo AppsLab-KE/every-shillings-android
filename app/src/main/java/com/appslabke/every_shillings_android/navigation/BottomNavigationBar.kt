94e96c968c38fa379768421859403eac0aa67096
moemaair <rageali12@gmail.com>
2023-05-22 12:52:32 +0300

    added Marketplace route

---
+++ app/src/main/java/com/appslabke/every_shillings_android/navigation/BottomNavigationBar.kt
@@ -0,0 +1,12 @@
+package com.appslabke.every_shillings_android.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun BottomNavigationBar(navController: NavController) {
+    val items = listOf(
+        Screens.Home,
+        Screens.
+    )
+}

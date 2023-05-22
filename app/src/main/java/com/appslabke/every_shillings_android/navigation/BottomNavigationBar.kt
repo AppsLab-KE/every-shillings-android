f0bc5bb82f557928000daaad750a90a558b253b0
moemaair <rageali12@gmail.com>
2023-05-22 13:14:58 +0300

    included icon param in the screens

--- app/src/main/java/com/appslabke/every_shillings_android/navigation/BottomNavigationBar.kt
+++ app/src/main/java/com/appslabke/every_shillings_android/navigation/BottomNavigationBar.kt
@@ -1,12 +1,51 @@
 package com.appslabke.every_shillings_android.navigation
 
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
 fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         Screens.Home,
-        Screens.
+        Screens.Marketplace,
+        Screens.ExchangeScreen,
+        Screens.Sell
     )
+
+    BottomNavigation (
+        backgroundColor = Color(0xFF2B5EC0),
+        contentColor = Color.White
+    ){
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach {item ->
+            BottomNavigationItem(
+               icon = { Icon(painter = painterResource(id = ), contentDescription = "icons")},
+                label = { Text (text = "")},
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.White.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route){
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(route){
+
+                            }
+                        }
+
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+        }
+    }
 }
--- app/src/main/java/com/appslabke/every_shillings_android/navigation/Screens.kt
+++ app/src/main/java/com/appslabke/every_shillings_android/navigation/Screens.kt
@@ -1,13 +1,19 @@
 package com.appslabke.every_shillings_android.navigation
 
-sealed class Screens (val route: String){
-    object Home: Screens(route = "Home")
-    object OnboardingScreen: Screens(route = "OnboardingScreen")
-    object SignUp: Screens(route = "SignUp")
-    object Login: Screens(route = "Login")
-    object LoginOtpScreen: Screens(route = "LoginOtpScreen")
-    object SignUpOtpScreen: Screens(route = "SignUpOtpScreen")
-    object TopUpScreen: Screens(route = "TopUpScreen")
-    object ExchangeScreen: Screens(route = "ExchangeScreen")
-    object Marketplace: Screens(route = "Marketplace")
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import com.appslabke.every_shillings_android.R
+
+sealed class Screens (val route: String, val icon: Int?){
+    object Home: Screens(route = "Home", icon = R.drawable.outline_home_24)
+    object OnboardingScreen: Screens(route = "OnboardingScreen", icon = null)
+    object SignUp: Screens(route = "SignUp", icon = null)
+    object Login: Screens(route = "Login", icon = null)
+    object LoginOtpScreen: Screens(route = "LoginOtpScreen", icon = null)
+    object SignUpOtpScreen: Screens(route = "SignUpOtpScreen", icon = null)
+    object TopUpScreen: Screens(route = "TopUpScreen", icon = null)
+    object ExchangeScreen: Screens(route = "ExchangeScreen", icon = R.drawable.outline_attach_money_24)
+    object Marketplace: Screens(route = "Marketplace", icon = R.drawable.outline_shopping_bag_24)
+    object Sell: Screens(route = "Sell", icon = R.drawable.outline_sell_24)
 }
